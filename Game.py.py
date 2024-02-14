8f9285d5c33de94e023bb96aee35e5444db369ea
chetnabarawal <159696405+chetnabarawal@users.noreply.github.com>
2024-02-14 15:06:10 +0530

    Add files via upload

---
+++ Game.py.py
@@ -0,0 +1,31 @@
+import random
+
+
+user_choice = int(input("Enter your choice: Type 0 for Rock, 1 for Paper, 2 for Scissors."))
+
+if user_choice >= 3 or user_choice < 0:
+    print("You entered a invalid number. You lose!!.")
+else:           
+    computer_choice = random.randint(0,2)
+
+    print("Computer Chose:")
+    print(computer_choice)
+
+
+    if computer_choice == user_choice:
+        print ("It's a draw.")
+
+    elif computer_choice == 0 and user_choice == 2:
+        print("You Lost !!.")
+
+    elif user_choice == 0  and computer_choice == 2:
+        print("You Win !!.")   
+            
+    elif computer_choice > user_choice:
+        print ("You Lost !!.")
+
+    elif user_choice > computer_choice:           
+        print("You Win!!.")     
+
+  
+         
